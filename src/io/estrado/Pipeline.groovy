183da441b50c4f529822fc4a0d31d6e72d466009
David Johnston <davidjohnston05@gmail.com>
2018-03-26 10:25:06 -0400

    Update withRegistry calls to include ecr string for aws ecr plugin

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -94,7 +94,8 @@ def containerBuildPub(Map args) {
 
     println "Running Docker build/publish: ${args.host}/${args.acct}/${args.repo}:${args.tags}"
 
-    docker.withRegistry("https://${args.host}", "${args.auth_id}") {
+    // updated for aws ecr plugin
+    docker.withRegistry("https://${args.host}", "ecr:us-east-1:${args.auth_id}") {
 
         // def img = docker.build("${args.acct}/${args.repo}", args.dockerfile)
         def img = docker.image("${args.acct}/${args.repo}")
